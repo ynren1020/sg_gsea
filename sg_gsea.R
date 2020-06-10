abcfe831ff104bb094d770dde179b73ef7833c70
ynren1020 <renxx275@umn.edu>
2020-06-10 15:40:08 -0500

    pre_gsea.R is used for producing data frame ready for single gene GSEA analysis. sg_gsea.R is the main function for single gene GSEA analysis, its output include enriched pathway data frame and an enrichment plot if pathway provided by user.

--- sg_gsea.R
+++ sg_gsea.R
@@ -1,9 +1,14 @@
+# example: 
+sg_gsea(tid_cohort = test, t_id = "T280760", cohort="PRAD", method = "logFC", 
+        geneset = paste0(dataDir, "/gmt/h.all.v7.0.symbols.gmt"),
+        pathway = NULL) #"HALLMARK_E2F_TARGETS"
 
-
-sg_gsea <- function(tid_cohort, method = "logFC", geneset, pathway){
+sg_gsea <- function(tid_cohort, t_id, method, cohort, geneset, pathway = NULL)
+    {
     # tid_cohort <- reflncT.tid.pradT
     
-    if (method = "logFC") {
+    if (method == "logFC") {
+    tid_cohort$group <- ifelse(tid_cohort[[2]] > median(tid_cohort[[2]]), "high", "low")
     # by group, calculate mean for each gene ---
     res <- aggregate(. ~ group, data = tid_cohort[, -c(1:2)], FUN = mean)
     rownames(res) <- res$group
@@ -26,12 +31,12 @@ sg_gsea <- function(tid_cohort, method = "logFC", geneset, pathway){
     } else {
         # correlation -----
         res <- NULL
-        res <- cor(tid_cohort[,1], tid_cohort[,3:ncol(tid_cohort)],
+        res <- cor(tid_cohort[,2], tid_cohort[,3:ncol(tid_cohort)],
                    use = 'pairwise.complete.obs')
         
         # make res ready for gsea -----
         res <- as.data.frame(t(as.data.frame(res)))
-        names(res) <- tid
+        names(res) <- t_id
         res$genes <- rownames(res)
         res <- res[ ,c(2,1)]
         res<-res[order(-res[,2]),] # cor decrease 
@@ -48,19 +53,22 @@ sg_gsea <- function(tid_cohort, method = "logFC", geneset, pathway){
      # tidy the results -----
     fgseaResTidy <- fgseaRes[order(-NES,padj),]
     print(fgseaResTidy)
-    data.table::fwrite(fgseaResTidy, paste0(tid, "_", strsplit(cohort,"[.]")[[1]][1], ".txt"),
+    data.table::fwrite(fgseaResTidy, paste0(t_id, "_", cohort, ".txt"),
                        col.names = TRUE, row.names = FALSE, sep = "\t", quote = FALSE)
     
     
     # gsea plot -----
-    #args5 <- "HALLMARK_ANDROGEN_RESPONSE"
-    #args5 <- "HALLMARK_MYC_TARGETS_V1"
-    #args5 <- NULL
+    #pathway <- "HALLMARK_ANDROGEN_RESPONSE"
+    #pathway <- "HALLMARK_MYC_TARGETS_V1"
+    #pathway <- NULL
     if(!is.null(pathway)){
-        pdf(file = paste0(pathway, "_", tid, ".pdf"))
-        fgsea::plotEnrichment(pathways.hallmark[[pathway]], ranks) + ggplot2::labs(title = pathway)}
+        pdf(file = paste0(pathway, "_", t_id, ".pdf"))
+        enrichplot <- fgsea::plotEnrichment(pathways.hallmark[[pathway]], ranks) + ggplot2::labs(title = pathway)
+        print(enrichplot)
+        }
     if(!is.null(pathway)){
+        
         dev.off()
-    }
-    
+   }
 }
+
--- test_git.R
+++ test_git.R
@@ -1,1 +1,79 @@
-print("This file is created within Rstudio")
+#print("This file is created within Rstudio")
+
+# test data.table::fread function for large data reading ---
+
+library(data.table)
+#dataDir <- path.expand("/Users/yren/Projects/sgGSEA")
+dataDir <- path.expand("~/Projects/sgGSEA")
+
+# interested cohort ---
+cohort <- as.data.frame(data.table::fread(paste0(dataDir, "/PRAD.FPKM.txt")))
+
+# filter out low expressed genes -----
+cohort <- cohort[rowMeans(cohort[,-1])>=1, ]
+rownames(cohort) <- cohort$GeneID
+cohort$GeneID <- NULL
+# transpose ---
+cohortT <- as.data.frame(t(cohort))
+cohortT <- data.frame(patient_id = row.names(cohortT), cohortT)
+# interested t_id ---
+t_id <- "ENST00000561519"
+t_id <- "T280760"
+reflnc <- data.table::fread(paste0(dataDir, "/RefLnc_lncRNA_tumor_sample_FPKM_tid"))
+mich <- data.table::fread(paste0(dataDir, "/mitranscriptome.expr.fpkm_tid.tsv"))
+
+if (any(grepl(t_id, reflnc[[1]]))) {
+dataFiles <- dir(dataDir, pattern = "FPKM$", full.names = TRUE)
+meta <- data.table::fread(paste0(dataDir, "/tcga.meta.file.txt"))
+# header ---
+command0 <- sprintf(
+    "grep t_name %s",
+    paste(dataFiles, collapse = " ")
+)
+} else if (any(grepl(t_id, mich[[1]]))) {
+    dataFiles <- dir(dataDir, pattern = "fpkm.tsv$", full.names = TRUE) 
+    meta <- data.table::fread(paste0(dataDir, "/library_info.txt"))
+    # header ---
+    command0 <- sprintf(
+        "grep transcript_id %s",
+        paste(dataFiles, collapse = " ")
+    )
+} else { message("Can not find the input t_id in the package database.")}
+
+
+command <- sprintf(
+    "grep %s",
+    paste(t_id, dataFiles, collapse = " ")
+)
+
+# create data.table with header ---
+header <- data.table::fread(cmd = command0)
+dt <- data.table::fread(cmd = command)
+names(dt) <- names(header)
+
+# transpose ---
+dt <- as.data.frame(dt)
+rownames(dt) <- dt[,1]
+dt[,1] <- NULL
+t_dt <- as.data.frame(t(dt))
+
+# test if sample name end with "gdc_realn_rehead"
+if (any(grepl("gdc_realn_rehead", rownames(t_dt)))){
+    t_dt$library_id <- stringr::str_split(rownames(t_dt),"_",simplify = TRUE)[,1]
+    t_dt.join <- dplyr::left_join(t_dt, meta, by = "library_id")
+    t_dt.join.sub <- t_dt.join[,c(3,1)]
+    tid_cohort <- na.omit(dplyr::full_join(t_dt.join.sub, cohortT,
+                                                 by = c("ALIQUOT_BARCODE" = "patient_id")))
+} else {
+    t_dt$library_id <- rownames(t_dt)
+    t_dt.join <- dplyr::left_join(t_dt, meta, by = "library_id")
+    t_dt.join.sub <- t_dt.join[,c("tcga_legacy_sample_id",t_id)]
+    tid_cohort <- na.omit(dplyr::full_join(t_dt.join.sub, cohortT,
+                                           by = c("tcga_legacy_sample_id" = "patient_id")))
+}
+
+
+
+
+
+
